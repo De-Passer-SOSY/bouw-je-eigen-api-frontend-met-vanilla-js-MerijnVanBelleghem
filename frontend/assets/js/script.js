d97ac35d0219c94294f372cfc1c87b5e108acf73
MerijnVanBelleghem <merijn.vanbelleghem@leerling.de-passer.be>
2025-05-30 23:07:29 +0200

    added form functionality

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -6,3 +6,17 @@ function init(){
     fetchPianos();
     console.log("Website ingeladen")
 
+    //knoppen toevoegen
+    const form = document.getElementById('piano-form');
+    const formWrapper = document.getElementById('form-wrapper');
+    const openFormBtn = document.getElementById('open-form-btn');
+
+    // Functionaliteit voor formulier om nieuwe piano toe te voegen
+    openFormBtn.addEventListener('click', () => {
+        formWrapper.classList.remove('hidden');
+        formWrapper.scrollIntoView({behavior: 'smooth'});
+        resetForm()
+
+    });
+    form.addEventListener('submit', handleFormSubmit);
+}
